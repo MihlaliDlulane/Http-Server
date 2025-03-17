a32afcff25fd863fc50c5c4657fa1cfa40379444
mihlali800 <Mihlali800@gmail.com>
2025-03-17 21:27:00 +0200

    completed files code

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
 #[allow(unused_imports)]
 use std::net::{TcpListener,TcpStream};
 use std::io::{Write,BufReader,BufRead};
+use std::{env,fs};
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
-    //
+    
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     
     for stream in listener.incoming() {
@@ -59,6 +56,24 @@ fn handle_client(mut stream:TcpStream) {
             let user_agent_vec: Vec<_> = user_agent.unwrap().split(":").collect();
             let message = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",user_agent_vec[1].len(),user_agent_vec[1]);
             response = message;
+        }
+        "files" => {
+            let Some(file_name) = index.get(2) else {panic!("Need file name!")};
+            let env_args: Vec<String> = env::args().collect();
+            let mut dir = env_args[2].clone();
+            dir.push_str(&file_name);
+
+            let file = fs::read(dir);
+
+            match file {
+                Ok(content) => {
+                    let message = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",content.len(),String::from_utf8(content).expect("file content"));
+                    response = message;
+                }
+                Err(_e) => {
+                    response = "HTTP/1.1 400 FILE NOT FOUND\r\n\r\n".to_string();
+                }
+            }
         }
         _ => {response = "HTTP/1.1 400 NOT FOUND\r\n\r\n".to_string();}
     }
